bf599ce89e0239e8d01d148925192404612cad5a
Zbigniew23 <36309387+Zbigniew23@users.noreply.github.com>
2018-02-16 22:06:33 +0100

    I add some comment

--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,13 @@
 public class Palindrome
 {
-  static int x = 11122111;
-  static int y = 11555789211;
+  static int s = 11122111;
+  static int y = 33555789255;
   static int z = 171;
-	
+	// some comments
 	public static void main(String[] args)
 	{
 		Palindrom3 pal = new Palindrom3();
-		 System.out.println(Palindrom3.isPalindrome(x));
+		 System.out.println(Palindrom3.isPalindrome(s));
 		 System.out.println(Palindrom3.isPalindrome(y));
 		 System.out.println(Palindrom3.isPalindrome(z));
 	}
