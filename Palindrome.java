b9ffd89cf6023dd6822b0d5cb5704ae892fbb644
Zbigniew23 <36309387+Zbigniew23@users.noreply.github.com>
2018-02-17 16:45:14 +0100

    some code

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,15 @@
 public class Palindrome
 {
-  static int d = 11122111;
+  static int t = 11122111;
   static int y = 33555789255;
   static int z = 171;
 	// some comments  
   // add comment
+  // add comment
 	public static void main(String[] args)
 	{
 		Palindrom3 pal = new Palindrom3();
-		 System.out.println(Palindrom3.isPalindrome(d));
+		 System.out.println(Palindrom3.isPalindrome(t));
 		 System.out.println(Palindrom3.isPalindrome(y));
 		 System.out.println(Palindrom3.isPalindrome(z));
 	}
